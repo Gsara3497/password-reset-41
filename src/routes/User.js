9b28d8c0cc9d8cac72f821cdbef794d71f489f78
SumaiyaBanu15 <sumaiyanishar115@gmail.com>
2023-12-01 19:11:56 +0530

    Code_Changed

--- src/routes/User.js
+++ src/routes/User.js
@@ -5,7 +5,7 @@ import Auth from '../common/auth.js'
 const router = express.Router()
 
 router.get('/',UserController.getEmail)
-router.post('/',UserController.postEmail)
+router.post('/signup',UserController.postEmail)
 router.post('/login',UserController.loginEmail)
 router.get('/data',Auth.validate,UserController.verifyToken)
 router.post('/reset-password',UserController.resetPassword)
